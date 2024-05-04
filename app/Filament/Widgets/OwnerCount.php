1eda40e5d555b74d8f1700f70ff9f589e6738aa7
Savio Fernandes <savio.vincent@gmail.com>
2024-05-04 18:44:28 +0400

    Widget

---
+++ app/Filament/Widgets/OwnerCount.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Owner;
+use App\Models\Tenant;
+use App\Models\Contract;
+use App\Models\Property;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Illuminate\Database\Eloquent\Builder;
+
+
+class OwnerCount extends BaseWidget
+{
+    protected function getStats(): array
+    {
+        return [
+            stat::make('Total Owners', Owner::count())
+                ->description('No of Owners')
+                ->descriptionIcon('heroicon-o-sparkles')
+                ->color('Success'),
+
+            stat::make('Total Properties', Property::count())
+                ->description('No of Property')
+                ->descriptionIcon('heroicon-o-sparkles')
+                ->color('Success'),
+
+            stat::make('Total Tenants', Tenant::count())
+                ->description('No of Property')
+                ->descriptionIcon('heroicon-o-sparkles')
+                ->color('Success'),
+
+            stat::make('Leased Properties', Property::where('status', 'LEASED')->count())
+                ->description('No of Property')
+                ->descriptionIcon('heroicon-o-sparkles')
+                ->color('Success'),
+
+            stat::make('Vacant Properties', Property::where('status', 'VACANT')->count())
+                ->description('No of Property')
+                ->descriptionIcon('heroicon-o-sparkles')
+                ->color('Success'),
+
+            $stat = Stat::make('Total Amount for Valid Contracts', Contract::where('validity', 'YES')->sum('amount')),
+
+
+        ];
+    }
+}
